ab371937f35e67bd23a67b043c6f59db20311705
Travis Emery <travis.emery@perficient.com>
2015-11-03 08:11:12 -0500

    Removing the to rename migrations since the tables don't need to be renames, they're created as-is

--- db/migrate/20151029164722_rename_payment_method_to_purchase.rb
+++
@@ -1,5 +0,0 @@
-class RenamePaymentMethodToPurchase < ActiveRecord::Migration
-	def change
-		rename_table :purchase, :payment_methods
-	end
-end
--- db/migrate/20151029170756_rename_purchase_to_payment_methods.rb
+++
@@ -1,5 +0,0 @@
-class RenamePurchaseToPaymentMethods < ActiveRecord::Migration
-  def change
-  	rename_table :purchase, :payment_methods
-  end
-end
